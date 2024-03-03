df199011406fbfd60201a32003ae951d822075e9
Sahkitt <jacksonmukamba68@gmail.com>
2024-03-03 17:55:35 +0300

    First commit

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const signupLink = document.querySelector('.signup-link');
-    const signinLink = document.querySelector('.signin-link');
-    const signinForm = document.querySelector('.signin');
-    const signupForm = document.querySelector('.signup');
+    const signupLink = document.querySelector('.signup-link');///fetch class signup-link
+    const signinLink = document.querySelector('.signin-link');///fetch class signin-link
+    const signinForm = document.querySelector('.signin');////fetch class signin
+    const signupForm = document.querySelector('.signup');///fetch class signup
   
+
+    // ////event listener when user clicks signup
     signupLink.addEventListener('click', function (event) {
       event.preventDefault();
-      signinForm.style.display = 'none';
-      signupForm.style.display = 'block';
+      signinForm.style.display = 'none';///signin is hidden 
+      signupForm.style.display = 'block';///signup is displayed
     });
   
+    ////// event listener when user clicks signin
     signinLink.addEventListener('click', function (event) {
       event.preventDefault();
-      signupForm.style.display = 'none';
-      signinForm.style.display = 'block';
+      signupForm.style.display = 'none';///signup is hidden
+      signinForm.style.display = 'block';///signin is displayed
     });
   });
